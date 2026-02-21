3cd5af749a232da4219c6c33a01f3de6d33fc12d
Maximilian Reutter <maximilianreutter@gmail.com>
2026-02-21 15:13:18 +0100

    pytest and flake8 works

--- app/main.py
+++ app/main.py
@@ -1,3 +1,56 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, dist: int | float | Distance) -> Distance:
+        if isinstance(dist, Distance):
+            return Distance(self.km + dist.km)
+        else:
+            return Distance(self.km + dist)
+
+    def __iadd__(self, dist: int | float | Distance) -> Distance:
+        if isinstance(dist, Distance):
+            self.km += dist.km
+        else:
+            self.km += dist
+        return self
+
+    def __mul__(self, mul: float) -> Distance:
+        return Distance(self.km * mul)
+
+    def __truediv__(self, div: float) -> Distance:
+        return Distance(round((self.km / div), 2))
+
+    def __lt__(self, dist: float) -> bool:
+        if self.km < dist:
+            return True
+        return False
+
+    def __gt__(self, dist: float) -> bool:
+        if self.km > dist:
+            return True
+        return False
+
+    def __eq__(self, dist: float) -> bool:
+        if self.km == dist:
+            return True
+        return False
+
+    def __le__(self, dist: float) -> bool:
+        if self.km <= dist:
+            return True
+        return False
+
+    def __ge__(self, dist: float) -> bool:
+        if self.km >= dist:
+            return True
+        return False
