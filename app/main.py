78e09a3048ee528dd25645001054b2d6582124cf
Maximilian Reutter <maximilianreutter@gmail.com>
2026-02-22 17:47:14 +0100

    remove else: in __iadd

--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ class Distance:
     def __iadd__(self, dist: int | float | Distance) -> Distance:
         if isinstance(dist, Distance):
             self.km += dist.km
-        else:
-            self.km += dist
+            return self
+        self.km += dist
         return self
 
     def __mul__(self, mul: int | float) -> Distance:
